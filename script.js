278922b8ae10b60b35363208b416e2afdaab83d9
Adewole Adebayo <wole1105@gmail.com>
2022-05-09 03:27:21 -0700

    added pictures, added buttons for pics

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+let img = document.querySelector("img");
+let btn1 = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2");
+let btn3 = document.querySelector("#btn3");
+let btn4 = document.querySelector("#btn4");
+let btn5 = document.querySelector("#btn5");
+let btn6 = document.querySelector("#btn6");
+let btn7 = document.querySelector("#btn7");
+let btn8 = document.querySelector("#btn8");
+let btn9 = document.querySelector("#btn9");
 
+btn1.addEventListener('click', () => {
+    img.src = "./images/jamesAndLily.jpeg";
+})
+
+btn2.addEventListener('click', () => {
+    img.src = "./images/James4.jpeg";
+})
+
+btn3.addEventListener('click', () => {
+    img.src = "./images/Lily2.jpg";
+})
+
+btn4.addEventListener('click', () => {
+    img.src = "./images/James2.jpeg";
+})
+
+btn5.addEventListener('click', () => {
+    img.src = "./images/jamesAndLily3.jpeg";
+})
+
+btn6.addEventListener('click', () => {
+    img.src = "./images/Lily.jpg";
+})
+
+btn7.addEventListener('click', () => {
+    img.src = "./images/James3.jpeg";
+})
+
+btn8.addEventListener('click', () => {
+    img.src = "./images/Lily3.jpeg";
+})
+
+btn9.addEventListener('click', () => {
+    img.src = "./images/jamesAndLily2.jpeg";
+})
